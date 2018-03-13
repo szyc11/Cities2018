c1896498e30a24b069ed94a7372139a41e68a6a8
Student <Student@193.107.34.205>
2018-03-13 12:06:35 +0100

    countries

---
+++ Cities2018/src/cities2018/Cities.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cities2018;
+
+import java.util.*;
+
+/**
+ *
+ * @author Student
+ */
+public class Cities {
+    private Map<String,List<String>> provinces = new HashMap();
+    
+    public Cities() {
+        List<String> war_miasta = new ArrayList<String>();
+        war_miasta.add("Olsztyn");
+        war_miasta.add("Elblag");
+        provinces.put("Warminsko-Mazurskie",war_miasta);
+        List<String> pom_miasta = new ArrayList<String>();
+        pom_miasta.add("Gdansk");
+        pom_miasta.add("Malbork");
+        
+    }
+    
+    
+   public static void main
+}
